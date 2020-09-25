e8888cf3f0c73e88aae2cf9bb8d019637fa6712e
Sudipto Ghosh <sudipto@ghosh.pro>
2020-09-25 14:16:50 +0000

    added bst implementation

---
+++ binaryTrees/main.cpp
@@ -0,0 +1,327 @@
+/**
+ *  Write a menu driven program to implement the following operations 
+ *  in a Binary Search Tree:
+ *    (a) Insertion
+ *    (b) Searching a node
+ *    (c) Display its preorder, postorder and inorder traversals. (recursive)
+ *    (d) Display its preorder, postorder and inorder traversals. (iterative)
+ *    (e) Display level-by-level traversal. (BFS)
+ *    (f) Create a mirror image of tree
+ *    (g) Count the non-leaf, leaf and total number of nodes  
+ *    (h) Search for an element x in the BST and change its value to y 
+ *        and then place the node with value y at its appropriate position
+ *    (i) Display height of tree
+ *    (j) Perform deletion by merging
+ *    (k) Perform deletion by copying
+ * 
+ *  Written by Sudipto Ghosh for the University of Delhi
+ */
+
+#include "stack.hpp"
+
+void getch();
+void clrscr();
+
+template <class T>
+class Node
+{
+public:
+  T data;
+  Node *left, *right;
+  Node()
+  {
+    left = nullptr;
+    right = nullptr;
+  }
+};
+
+class BinarySearchTree
+{
+public:
+  Node<int> *root;
+  Stack<Node<int> *> stack1, stack2;
+
+  BinarySearchTree()
+  {
+    root = nullptr;
+  }
+
+  void insert(int data, Node<int> *current)
+  {
+    Node<int> *temp;
+    if (root == nullptr)
+    {
+      root = new Node<int>;
+      root->data = data;
+      root->left = root->right = nullptr;
+    }
+    else
+    {
+      if ((data < current->data) &&
+          (current->left == nullptr))
+      {
+        temp = new Node<int>;
+        temp->data = data;
+        temp->left = temp->right = nullptr;
+        current->left = temp;
+      }
+      else if ((data > current->data) &&
+               (current->right == nullptr))
+      {
+        temp = new Node<int>;
+        temp->data = data;
+        temp->left = temp->right = nullptr;
+        current->right = temp;
+      }
+      else
+      {
+        if (data < current->data)
+          insert(data, current->left);
+        else
+          insert(data, current->right);
+      }
+    }
+  }
+
+  Node<int> *search(int key, Node<int> *root)
+  {
+    if (root == nullptr || root->data == key)
+      return root;
+    if (root->data < key)
+      return search(key, root->right);
+    return search(key, root->left);
+  }
+
+  void inOrderRecursive(Node<int> *root)
+  {
+    if (root != nullptr)
+    {
+      inOrderRecursive(root->left);
+      cout << root->data << " ";
+      inOrderRecursive(root->right);
+    }
+  }
+
+  void preOrderRecursive(Node<int> *root)
+  {
+    if (root != nullptr)
+    {
+      cout << root->data << " ";
+      preOrderRecursive(root->left);
+      preOrderRecursive(root->right);
+    }
+  }
+
+  void postOrderRecursive(Node<int> *root)
+  {
+    if (root != nullptr)
+    {
+      postOrderRecursive(root->left);
+      postOrderRecursive(root->right);
+      cout << root->data << " ";
+    }
+  }
+
+  void inOrderIterative()
+  {
+    Node<int> *current = root;
+
+    while (current != nullptr ||
+           stack1.isEmpty() == false)
+    {
+      while (current != nullptr)
+      {
+        stack1.push(current);
+        current = current->left;
+      }
+      current = stack1.pop();
+      cout << current->data << " ";
+      current = current->right;
+    }
+  }
+
+  void preOrderIterative()
+  {
+    Node<int> *node, *temp = root;
+    if (temp == nullptr)
+      return;
+    stack1.push(temp);
+    while (stack1.isEmpty() == false)
+    {
+      node = stack1.pop();
+      cout << node->data << " ";
+      if (node->right)
+        stack1.push(node->right);
+      if (node->left)
+        stack1.push(node->left);
+    }
+  }
+
+  void postOrderIterative()
+  {
+    Node<int> *temp = root;
+    if (temp == nullptr)
+      return;
+    do
+    {
+      while (temp)
+      {
+        if (temp->right)
+          stack1.push(temp->right);
+        stack1.push(temp);
+        temp = temp->left;
+      }
+      temp = stack1.pop();
+      if (temp->right && !stack1.isEmpty() &&
+          stack1.top() == temp->right)
+      {
+        stack1.pop();
+        stack1.push(temp);
+        temp = temp->right;
+      }
+      else
+      {
+        cout << temp->data << " ";
+        temp = nullptr;
+      }
+    } while (!stack1.isEmpty());
+  }
+
+  int levelByLevelTraversal()
+  {
+    Node<int> *current = root;
+    stack1.push(current);
+
+    while (!(stack1.isEmpty()) ||
+           !(stack2.isEmpty()))
+    {
+      if (!stack1.isEmpty())
+        while (!stack1.isEmpty())
+        {
+          current = stack1.pop();
+          cout << current->data << " ";
+          if (current->right != nullptr)
+            stack2.push(current->right);
+          if (current->left != nullptr)
+            stack2.push(current->left);
+        }
+      else
+        break;
+      cout << endl;
+
+      if (!stack2.isEmpty())
+        while (!stack2.isEmpty())
+        {
+          current = stack2.pop();
+          cout << current->data << " ";
+          if (current->right != nullptr)
+            stack1.push(current->right);
+          if (current->left != nullptr)
+            stack1.push(current->left);
+        }
+      else
+        break;
+      cout << endl;
+    }
+  }
+};
+
+int main(void)
+{
+  int choice, data;
+  BinarySearchTree tree;
+
+  do
+  {
+    cout << "      MENU      \n"
+         << "================\n"
+         << "(1) Insertion\n"
+         << "(2) Searching a node\n"
+         << "(3) Display its preorder, postorder and inorder traversals. (recursive)\n"
+         << "(4) Display its preorder, postorder and inorder traversals. (iterative)\n"
+         << "(5) Display level-by-level traversal. (BFS)\n"
+         << "(6) Create a mirror image of tree\n"
+         << "(7) Count the non-leaf, leaf and total number of nodes  \n"
+         << "(8) Search for an element x in the BST and change its value to y \n"
+         << "    and then place the node with value y at its appropriate position\n"
+         << "(9) Display height of tree\n"
+         << "(10) Perform deletion by merging\n"
+         << "(11) Perform deletion by copying\n"
+         << "(0) Exit\n\n";
+    cout << "Enter Choice: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+      cout << "\nEnter Node Data: ";
+      cin >> data;
+      tree.insert(data, tree.root);
+      break;
+    case 2:
+      cout << "\nEnter Search Data: ";
+      cin >> data;
+      cout << "Search Result: ";
+      if (tree.search(data, tree.root))
+        cout << "Found";
+      else
+        cout << "Not Found";
+      cout << endl;
+      break;
+    case 3:
+      cout << endl;
+      cout << "In-Order Recursive Traversal: ";
+      tree.inOrderRecursive(tree.root);
+      cout << endl;
+      cout << "Pre-Order Recursive Traversal: ";
+      tree.preOrderRecursive(tree.root);
+      cout << endl;
+      cout << "Post-Order Recursive Traversal: ";
+      tree.postOrderRecursive(tree.root);
+      cout << endl;
+      break;
+    case 4:
+      cout << endl;
+      cout << "In-Order Iterative Traversal: ";
+      tree.inOrderIterative();
+      cout << endl;
+      cout << "Pre-Order Iterative Traversal: ";
+      tree.preOrderIterative();
+      cout << endl;
+      cout << "Post-Order Iterative Traversal: ";
+      tree.postOrderIterative();
+      cout << endl;
+      break;
+    case 5:
+      cout << endl;
+      cout << "Level-by-level Traversal: \n";
+      tree.levelByLevelTraversal();
+      break;
+    case 0:
+    default:
+      break;
+    }
+    getch();
+    clrscr();
+  } while (choice != 0);
+
+  return 0;
+}
+
+void getch()
+{
+  cout << "\nPress any key to continue...";
+  cin.ignore();
+  cin.get();
+  return;
+}
+
+void clrscr()
+{
+#ifdef _WIN32
+  system("cls");
+#elif __unix__
+  system("clear");
+#endif
+  return;
+}
